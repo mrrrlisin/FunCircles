ac60ab902b331cb96588bfe20098934c8d716a3f
mrrrlisin <33547742+mrrrlisin@users.noreply.github.com>
2017-11-24 23:39:11 +0300

    Add files via upload

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    Rigidbody2D myBody;
+    public float speed = 45.0f;
+
+	void Start ()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+        myBody.AddForce(new Vector2(horizontal, vertical));
+
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Gem")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
