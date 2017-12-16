0f4774bc5ac95db6ce65bc6b27382a5732c0f250
mrrrlisin <33547742+mrrrlisin@users.noreply.github.com>
2017-12-16 10:17:18 +0300

    Add files via upload

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject item;
+    public int count = 10;
+
+    GameObject map;
+
+    float x, y;
+    float maxX, maxY;
+
+	void Start ()
+    {
+        map = GameObject.FindGameObjectWithTag("Background");
+        var mapSprite = map.GetComponent<SpriteRenderer>();
+        var mapCollider = map.GetComponent<BoxCollider2D>();
+        var itemSprite = item.GetComponent<SpriteRenderer>();
+
+        float offset = Mathf.Min(mapCollider.size.x, mapCollider.size.y) + 
+            Mathf.Max(itemSprite.bounds.size.x, itemSprite.bounds.size.y) / 2;
+
+        maxX = mapSprite.sprite.bounds.size.x / 2 - offset;
+        maxY = mapSprite.sprite.bounds.size.y / 2 - offset;
+        Spawn(count);
+    }
+
+    void Spawn(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            x = Random.Range(-maxX, maxX);
+            y = Random.Range(-maxY, maxY);
+            Instantiate(item, new Vector3(x, y, 0), Quaternion.identity);
+        }
+    }
+	
+}
